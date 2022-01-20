5d604584431847d1b63d821f99b2a163aa5aa35b
Allen <ooswe@yahoo.com>
2022-01-20 15:49:52 -0500

    Generate test crash

--- CICDiOS/ViewController.swift
+++ CICDiOS/ViewController.swift
@@ -6,14 +6,42 @@
 //
 
 import UIKit
+import  AppCenterCrashes
 
 class ViewController: UIViewController {
 
+    let locationButton = UIButton()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemTeal
+        setup()
+        style()
+        layout()
     }
 
-
+    @objc func locationPressed(_ sender: UIButton) {
+        Crashes.generateTestCrash()
+    }
 }
 
+extension ViewController {
+    
+    func setup() {
+    }
+
+    func style() {
+        locationButton.translatesAutoresizingMaskIntoConstraints = false
+        locationButton.setBackgroundImage(UIImage(systemName: "location.circle.fill"), for: .normal)
+        locationButton.addTarget(self, action: #selector(locationPressed(_:)), for: .primaryActionTriggered)
+        locationButton.tintColor = .label
+    }
+
+    func layout() {
+        view.addSubview(locationButton)
+        
+        NSLayoutConstraint.activate([
+            locationButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            locationButton.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+}
